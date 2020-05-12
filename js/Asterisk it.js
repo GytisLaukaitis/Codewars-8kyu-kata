7e5a7e62cb110bfcc5c3e9e4ddf6b05c5b21b933
gytislaukaitisi <63237716+gytislaukaitisi@users.noreply.github.com>
2020-05-12 18:41:59 +0300

    g

---
+++ js/Asterisk it.js
@@ -0,0 +1,16 @@
+"use strict"
+
+function asteriscIt(n) {
+    let skaiciai = n.join('');
+    
+    
+
+
+
+
+return skaiciai;
+
+ };
+
+ console.log(asteriscIt([1, 4, 64, 68, 67, 23, 1]), '14*6*4*6*8*67231');
+ console.log(asteriscIt(5312708), '531270*8');
