b2e4f54d63ec0ea3ae4127abed15e3551b4df131
gytislaukaitisi <63237716+gytislaukaitisi@users.noreply.github.com>
2020-05-13 23:54:33 +0300

    gg

---
+++ js/Return the first M multiples of N.js
@@ -0,0 +1,12 @@
+"use strict"
+
+function multiples(m, n){
+    let arr=[];
+    for (let i=1; i<=m; ++i)
+      arr.push(n*i);
+    return arr;
+  }
+  
+
+
+console.log(multiples(3, 5), [5, 10, 15]);
