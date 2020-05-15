39684b8672f17fe329d1cb0fa6b2f378480e0495
gytislaukaitisi <63237716+gytislaukaitisi@users.noreply.github.com>
2020-05-15 15:41:04 +0300

    fina occurancies

---
+++ js/Find all occurrences of an element in an array.js
@@ -0,0 +1,14 @@
+"use strict"
+
+
+const findAll = (array, n) => {
+    let result = [];
+    for (let i = 0; i < array.length; i++) {
+    if ( array[i] === n){
+    result.push(i);
+   } 
+ }return result;
+}
+
+
+console.log(findAll([6, 9, 3, 4, 3, 82, 11], 3), [2, 4]);
